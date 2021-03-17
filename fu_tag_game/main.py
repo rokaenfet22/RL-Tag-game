4f94eaad59e378a7dc698b7823a880d342056d37
Fusuke Minagi <fussinboots22@gmail.com>
2021-03-17 21:45:16 +0900

    Outputs list of "walls" in console, added momentum

--- fu_tag_game/main.py
+++ fu_tag_game/main.py
@@ -2,7 +2,7 @@ from walls import gen_wall
 from tag import tag
 
 res = gen_wall()
-
+print(res)
 #modes of movement of seeker and runner changeable within code in tag.py
 
 if res:
--- fu_tag_game/tag.py
+++ fu_tag_game/tag.py
@@ -8,29 +8,26 @@ def tag(wall_rects):
     window=pygame.display.set_mode((window_x,window_y))
 
     #modes of movement, key, random, basic
-    seeker_move_mode="random"
-    runner_move_mode="key"
+    seeker_move_mode="key"
+    runner_move_mode="basic"
     #size of both
-    seeker_size=20
+    seeker_size=10
     runner_size=10
+    #momentum val
+    acceleration=0.5
 
-    class Seeker():
-        def __init__(self,x,y,size,color="black",velocity=1):
+    class Entity():
+        def __init__(self,x,y,size,color="black",name=""):
             self.coords=[x,y,size,size]
             self.prev_pos=[x,y]
             self.color=color
-            self.velocity=velocity
+            self.vx,self.vy=0,0
+            self.name=name
         def draw(self):
-            draw_rect(self.coords,self.color)
-
-    class Runner():
-        def __init__(self,x,y,size,color="black",velocity=1):
-            self.coords=[x,y,size,size]
-            self.prev_pos=[x,y]
-            self.color=color
-            self.velocity=velocity
-        def draw(self):
-            draw_rect(self.coords,self.color)
+            draw_rect(self.coords,self.color,0)
+        def move(self):
+            self.coords[0]+=self.vx
+            self.coords[1]+=self.vy
 
     def draw_rect(c,color="black",w=1):
         pygame.draw.rect(window,pygame.Color(color),(c[0],c[1],c[2],c[3]),w)
@@ -40,38 +37,51 @@ def tag(wall_rects):
             if ((rect1[1] <= rect2[1]+rect2[3]) and (rect1[1] >= rect2[1])) or ((rect1[1]+rect1[3] <= rect2[1]+rect2[3]) and (rect1[1]+rect1[3] >= rect2[1])):
                 return True
 
-    def out_of_bounds(r): #r = Rect
-        if r[0]<=0: r[0] = 0
-        elif r[0]+r[2]>=window_x: r[0]=window_x-r[2]
-        if r[1]<=0: r[1]=1
-        elif r[1]+r[3]>=window_y: r[1]=window_y-r[3]
+    def out_of_bounds(i): #r = Rect
+        r=i.coords
+        x=False
+        y=False
+        if r[0]<=0: r[0] = 0; x=True
+        elif r[0]+r[2]>=window_x: r[0]=window_x-r[2]; x=True
+        if r[1]<=0: r[1]=1; y=True
+        elif r[1]+r[3]>=window_y: r[1]=window_y-r[3]; y=True
+        if x: i.vx=0
+        if y: i.vy=0
         return r
 
     def key_move(key,i): #key mode, i=instance
-        if key[pygame.K_w]: i.coords[1]-=i.velocity
-        elif key[pygame.K_s]: i.coords[1]+=i.velocity
-        if key[pygame.K_a]: i.coords[0]-=i.velocity
-        elif key[pygame.K_d]: i.coords[0]+=i.velocity
+        if key[pygame.K_w]: i.vy-=acceleration
+        elif key[pygame.K_s]: i.vy+=acceleration
+        if key[pygame.K_a]: i.vx-=acceleration
+        elif key[pygame.K_d]: i.vx+=acceleration
 
     def random_move(key,i): #random mode, 0=move in negative, 1=move in positive, 2= don't move
         r=randint(0,2)
-        if r==2: i.coords[1]-=i.velocity
-        elif r: i.coords[1]+=i.velocity
+        if r==2: i.vy-=acceleration
+        elif r: i.vy+=acceleration
         r=randint(0,2)
-        if r==2: i.coords[0]-=i.velocity
-        elif r: i.coords[0]+=i.velocity
+        if r==2: i.vx-=acceleration
+        elif r: i.vx+=acceleration
 
     def basic_move(key,a,b): #basic mode, a=moved instance, b=based instance
         [ax,ay]=a.coords[:2]
         [bx,by]=b.coords[:2]
-        #y
-        if ax<bx:a.coords[0]+=a.velocity
-        elif ax>bx:a.coords[0]-=a.velocity
-        #x
-        if ay<by:a.coords[1]+=a.velocity
-        elif ay>by:a.coords[1]-=a.velocity
+        if a.name=="seeker":
+            #y
+            if ax<bx:a.vx+=acceleration
+            elif ax>bx:a.vx-=acceleration
+            #x
+            if ay<by:a.vy+=acceleration
+            elif ay>by:a.vy-=acceleration
+        elif a.name=="runner":
+            #y
+            if ax<bx:a.vx-=acceleration
+            elif ax>bx:a.vx+=acceleration
+            #x
+            if ay<by:a.vy-=acceleration
+            elif ay>by:a.vy+=acceleration
 
-    def moves(key,s,r): #s=seeker, r=runner
+    def update(key,s,r): #s=seeker, r=runner
         if seeker_move_mode=="key":
             key_move(key,s)
         elif seeker_move_mode=="random":
@@ -86,21 +96,24 @@ def tag(wall_rects):
         elif runner_move_mode=="basic":
             basic_move(key,r,s)
 
-    #create instances of seeker and runner, generate in open space
+    #create instances of seeker and runner, generate in vacant space
     while True:
         c=[randint(0,window_x-seeker_size),randint(0,window_y-seeker_size)]
         if not any([rect_collision(c+[seeker_size,seeker_size],n) for n in wall_rects]):
             break
-    seeker=Seeker(c[0],c[1],seeker_size,"red")
+    seeker=Entity(c[0],c[1],seeker_size,"red","seeker")
     while True: 
         c = [randint(0,window_x-runner_size),randint(0,window_y-runner_size)]
         if not any([rect_collision(c+[runner_size,runner_size],n) for n in wall_rects+[seeker.coords]]):
             break
-    runner=Runner(c[0],c[1],10,"green")
+    runner=Entity(c[0],c[1],runner_size,"green","runner")
 
     run=True
     caught=False
+    clock=pygame.time.Clock()
     while run:
+        dt=clock.tick(30)
+        #dt=how many ms since last iteration/frame i.e. 30fps atm
         pygame.time.delay(3)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -113,24 +126,31 @@ def tag(wall_rects):
         seeker.prev_pos=seeker.coords[:2]
         runner.prev_pos=runner.coords[:2]
 
-        key=pygame.key.get_pressed()
-        moves(key,seeker,runner)
+        #update velocity of each entity
+        update(pygame.key.get_pressed(),seeker,runner)
 
-        #bound within screen
-        seeker.coords=out_of_bounds(seeker.coords)
-        runner.coords=out_of_bounds(runner.coords)
+        #move entities
+        seeker.move()
+        runner.move()
 
-        #collided into a wall
+        #bound within screen, momentum reset if border hit
+        seeker.coords=out_of_bounds(seeker)
+        runner.coords=out_of_bounds(runner)
+
+        #wall collision, momentum reset if wall hit
         for n in wall_rects:
             if rect_collision(seeker.coords,n):
                 t=seeker.coords
                 if rect_collision([seeker.prev_pos[0]]+t[1:],n): #if reverting x fails
                     if rect_collision([t[0]]+[seeker.prev_pos[1]]+t[2:],n): #if reverting y fails
-                        seeker.coords=seeker.prev_pos+[seeker_size,seeker_size]
-                    else:
+                        seeker.coords=seeker.prev_pos+[seeker_size,seeker_size] #revert both
+                        seeker.vx,seeker.vy=0,0
+                    else: #revert only y
                         seeker.coords=[t[0],seeker.prev_pos[1],seeker_size,seeker_size]
-                else:
+                        seeker.vy=0
+                else: #revert only x
                     seeker.coords=[seeker.prev_pos[0],t[1],seeker_size,seeker_size]
+                    seeker.vx=0
                 break
         
         for n in wall_rects:
@@ -138,13 +158,16 @@ def tag(wall_rects):
                 t=runner.coords
                 if rect_collision([runner.prev_pos[0]]+t[1:],n): #if reverting x fails
                     if rect_collision([t[0]]+[runner.prev_pos[1]]+t[2:],n): #if reverting y fails
-                        runner.coords=runner.prev_pos+[runner_size,runner_size]
-                    else:
+                        runner.coords=runner.prev_pos+[runner_size,runner_size] #revert both
+                        runner.vx,runner.vy=0,0
+                    else: #revert y
                         runner.coords=[t[0],runner.prev_pos[1],runner_size,runner_size]
-                else:
+                        runner.vy=0
+                else: #rvert x
                     runner.coords=[runner.prev_pos[0],t[1],runner_size,runner_size]
+                    runner.vx=0
 
-        #check collision between seeker and runner
+        #check runner being caught
         if rect_collision(seeker.coords,runner.coords):
             caught=True
             run=False #quit game when caught for now
